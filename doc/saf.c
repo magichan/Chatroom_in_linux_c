93c3368669942bf66a7efd8ebb19bb511688570c
magichan <574932286@qq.com>
2015-01-25 23:04:15 +0800

    开发技术文档建立

---
+++ doc/saf.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  saf.c
+ *
+ *    Description:  :
+ *
+ *        Version:  1.0
+ *        Created:  2015年01月25日 23时03分21秒
+ *       Revision:  none
+ *       Compiler:  clang 
+ *
+ *         Author:  magichan (fgm), 574932286@qq.com
+ *   Organization:  FH Südwestfalen, Iserlohn
+ *           Rule:  ???? question 
+ *                  [    comment
+ *
+ * =====================================================================================
+ */
+
+
